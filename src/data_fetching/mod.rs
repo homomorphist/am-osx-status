5d4d4e0a7c4991cf4b8b72f6ba0c46f5e611f4e7
agapurnis <agapurnis@outlook.com>
2025-05-18 19:23:45 -0700

    log ids on data fetch error; reformat; remove util

--- src/data_fetching/mod.rs
+++ src/data_fetching/mod.rs
@@ -3,8 +3,6 @@ pub mod services;
 use components::{image::TrackImageUrlPack, Component, ComponentSolicitation};
 use services::{artworkd::{get_artwork, StoredArtwork}, itunes::ITunesStoreSong};
 
-use crate::util::fallback_to_default_and_log_error;
-
 pub mod components;
 
 #[derive(Debug)]
@@ -21,32 +19,36 @@ impl AdditionalTrackData {
     ) -> Self {
         let mut itunes: Option<ITunesStoreSong> = None;
         let mut images = TrackImageUrlPack::none();
+        let id = track.persistent_id.as_str();
 
         if solicitation.list.contains(&Component::ArtistImage) {
             if let Some(db) = musicdb {
-                let id = musicdb::PersistentId::try_from(track.persistent_id.as_str()).expect("bad id");
+                let id = musicdb::PersistentId::try_from(id).expect("bad id");
                 images.artist = db.tracks().get(&id)
                     .and_then(|track| db.get(track.artist_id))
                     .and_then(|artist| artist.artwork_url.as_ref())
-                    .and_then(|artwork| {
-                        if artwork.parameters.effect == Some(mzstatic::image::effect::Effect::SquareFitCircle) {
-                            None // ugly auto-generated
-                        } else {
-                            Some(artwork.to_string())
-                        }
-                    });
+                    .filter(|artwork| artwork.parameters.effect != Some(mzstatic::image::effect::Effect::SquareFitCircle)) // ugly auto-generated
+                    .map(|artwork| artwork.to_string())
             }
         }
 
         if solicitation.list.contains(&Component::AlbumImage) {
-            images.track = match crate::util::fallback_to_default_and_log_error!(get_artwork(&track.persistent_id).await) {
+            let artwork = match get_artwork(id).await {
+                Ok(artwork) => artwork,
+                Err(err) => {
+                    tracing::error!(?err, %id, "failed to get artwork");
+                    None
+                }
+            };
+
+            images.track = match artwork {
                 None => None,
                 Some(artwork) => match artwork {
                     StoredArtwork::Remote { url } => Some(url),
                     StoredArtwork::Local { path } => {
                         if let Some(host) = host  {
                             host.for_track(track, &path).await.inspect_err(|err| {
-                                tracing::error!(?err, "failed to upload custom artwork");
+                                tracing::error!(?err, %id, "failed to upload custom artwork");
                             }).ok()
                         } else {
                             None
@@ -57,7 +59,13 @@ impl AdditionalTrackData {
         }
 
         if solicitation.list.contains(&Component::ITunesData) {
-            itunes = fallback_to_default_and_log_error!(services::itunes::find_track(track).await);
+            itunes = match services::itunes::find_track(track).await {
+                Ok(itunes) => itunes,
+                Err(err) => {
+                    tracing::error!(?err, %id, "failed to get iTunes data");
+                    None
+                }
+            }
         }
 
         Self {
--- src/util.rs
+++ src/util.rs
@@ -1,23 +1,3 @@
-macro_rules! fallback_to_default_and_log_error {
-    ($result: expr) => {
-        {
-            // needed to get T::default
-            fn fallback_to_default_and_log_error<T: Default, E: core::fmt::Debug>(result: Result<T, E>) -> T {
-                match result {
-                    Ok(value) => value,
-                    Err(error) => {
-                        tracing::error!(?error, "failed to get value; using default");
-                        T::default()
-                    }
-                }
-            }
-            fallback_to_default_and_log_error($result)
-        }
-    };
-}
-
-pub(crate) use fallback_to_default_and_log_error;
-
 pub const REPOSITORY_URL: &str = "https://github.com/Agapurnis/am-osx-status";
 
 use std::sync::LazyLock;
