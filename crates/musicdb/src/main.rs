7f4e8cc594021c2f6f219d1ffa19e80d27a170e3
agapurnis <agapurnis@outlook.com>
2026-01-09 18:21:39 -0800

    feat(musicdb): add standalone cli feature variant with tracing-subscriber logger

--- crates/musicdb/src/main.rs
+++ crates/musicdb/src/main.rs
@@ -2,6 +2,8 @@
 pub use musicdb::*;
 
 fn main() {
+    #[cfg(feature = "cli-standalone")]
+    tracing_subscriber::fmt::init();
     use clap::Parser;
     use cli::Arguments;
     Arguments::parse().command.handle();
