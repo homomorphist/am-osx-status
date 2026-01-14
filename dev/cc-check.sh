0918f605c0877a36976ee5575d12846e97c44678
agapurnis <agapurnis@outlook.com>
2026-01-13 17:50:16 -0800

    fix(dev): use git stripspace for extracting commit msgc in commit hook

--- dev/cc-check.sh
+++ dev/cc-check.sh
@@ -14,7 +14,7 @@ if [ $# -eq 0 ]; then
   exit 0
 fi
 
-COMMIT_MSG=$(cat "$1" | sed '/^#/d' | sed '/^$/d') # strip git comments
+COMMIT_MSG=$(git stripspace -s < "$1")
 
 ALLOWED_TYPES="build,chore,ci,docs,feat,fix,perf,refactor,revert,style,test" # from @commitlint/config-conventional
 ALLOWED_SCOPES="db,dev,log,ipc,osa,service,dispatch,discord,lastfm,brainz,musicdb,mzstatic,utf16,xml,plist"
